5fc70a305063a7918c5a6635cbe9ce68de55db2d
Umut Berhan <umutberhan94@gmail.com>
2024-12-01 23:47:34 +0300

    exclude sourcemap on bundling

--- esbuild.config.js
+++ esbuild.config.js
@@ -5,7 +5,7 @@ esbuild.build({
     bundle: true,
     outfile: 'out/extension.js', // Output bundled file
     minify: true,
-    sourcemap: true,
+    sourcemap: false,
     platform: 'node',  // Ensures compatibility with Node.js environment
     target: 'es2020',
     external: ['vscode'], // Exclude vscode from the bundle
